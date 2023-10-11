c411f80eeeb274b087c4233bd4d920caa32fe9bc
jertonmal <jertonmal@gmail.com>
2023-10-11 14:44:27 -0300

    Mensagem

--- contact/models.py
+++ contact/models.py
@@ -10,7 +10,7 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
     
     def __str__(self) -> str:
